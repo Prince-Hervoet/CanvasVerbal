39a60c4f555dd83a4870eda4474147365188100e
Prince-Hervoet <2064128441@qq.com>
2023-04-28 16:46:11 +0800

    fix: #modify

--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,6 @@ import { Rect } from "./../code/type/Rect.js";
 import { canvasVerbal } from "../code/vo/CanvasVerbal.js";
 const big = document.getElementById("big")!;
 const cv = canvasVerbal("asdf", 800, 800, { "background-color": "#ccc" }, big);
-// const cv2 = canvasVerbal("234", 800, 800, { "background-color": "blue" }, big!);
 
 const test = new Rect({
   width: 100,
